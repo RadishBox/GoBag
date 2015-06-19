32acab3a05db31e2b61bbe238c422e484e3029eb
Pablo Mendizábal Mondragón <p.mendizabal.m@gmail.com>
2015-06-19 18:55:54 -0500

    +Textures and animations
    Added and fixed atlases, progress with character animation.

---
+++ Assets/PabloControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PabloControllerTest : MonoBehaviour {
+
+	Animator anim;
+	public Animator umbrella;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical") > 0f) {
+			anim.SetBool ("isUp", true);
+			umbrella.SetBool ("isUp2", true);
+		} else if (Input.GetAxis ("Vertical") < 1f) {
+			anim.SetBool ("isUp", false);
+			umbrella.SetBool ("isUp2", false);
+		}
+	}
+}
