11bb13feeaba9954bb270c6a72e5f1e7e63012d5
xipeluife <l.felipe.torres.p@gmail.com>
2015-08-25 00:42:31 -0500

    Linked spawned entities with level scenario

--- Assets/Scripts/ExploreGameController.cs
+++ Assets/Scripts/ExploreGameController.cs
@@ -5,7 +5,8 @@ using System.Collections.Generic;
 /// <summary>
 /// This class controls the exploration part of the game
 /// </summary>
-public class ExploreGameController : MonoBehaviour {
+public class ExploreGameController : MonoBehaviour
+{
 
 
     private static ExploreGameController _instance;
@@ -23,14 +24,16 @@ public class ExploreGameController : MonoBehaviour {
     // GUI prep
     public GameObject StatusGroup;
 
-    void Awake() {
+    void Awake()
+    {
         Instance = this;
     }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
         Initialize();
-	}
+    }
 
     void Initialize()
     {
@@ -59,17 +62,17 @@ public class ExploreGameController : MonoBehaviour {
     }
 
     /// <summary>
-        /* General turn 
-         *  1. Player uses up to 1 item
-         *  2. Check used item effects
-         *  3. Player moves
-         *  4. Check winning condition
-         *  5. Player movement effects
-         *  6. Scenario ambient effects
-         *  7. Other movable entities move
-         *  8. Other entity effects
-         *  9. Status bars get modified
-         */
+    /* General turn
+     *  1. Player uses up to 1 item
+     *  2. Check used item effects
+     *  3. Player moves
+     *  4. Check winning condition
+     *  5. Player movement effects
+     *  6. Scenario ambient effects
+     *  7. Other movable entities move
+     *  8. Other entity effects
+     *  9. Status bars get modified
+     */
     /// </summary>
     private IEnumerator AdvanceTurn()
     {
@@ -89,7 +92,7 @@ public class ExploreGameController : MonoBehaviour {
 
     // Turn loop for all entities
     private IEnumerator ProcessTurn()
-    {        
+    {
         _isTurnProcessing = true;
         print("Turn started");
         Turn++;
@@ -101,11 +104,11 @@ public class ExploreGameController : MonoBehaviour {
             yield return null;
 
         // Process other entities' turn
-        foreach (MapEntity entity in entities) 
+        foreach (MapEntity entity in entities)
         {
             entity.PlayTurn();
 
-            while(entity.InTurn)
+            while (entity.InTurn)
                 yield return null;
         }
 
@@ -121,40 +124,44 @@ public class ExploreGameController : MonoBehaviour {
 
         foreach (GameObject entity in gameEntities)
         {
-            // Decide number of said entity
-            int numberToSpawn = Random.Range(4, 5);
+            ScenarioLibrary.ScenarioType scenarioType = entity.GetComponent<MapEntity>().Scenario;
+            // Check if it belongs to that scenario
+            if ((scenarioType == ScenarioLibrary.ScenarioType.All) || (scenarioType == GameConfiguration.Instance.Level.scenario))
+            {
+                // Decide number of said entity
+                int numberToSpawn = Random.Range(4, 5);
 
-            for(int i = 0; i < numberToSpawn; i++)
-            {                
-                MapTile targetTile = Map.GetComponent<MapController>().GetRandomTile();
+                for (int i = 0; i < numberToSpawn; i++)
+                {
+                    MapTile targetTile = Map.GetComponent<MapController>().GetRandomTile();
 
-                GameObject entityGO = Instantiate(entity);
-                entityGO.transform.SetParent(Map.transform);
-                entityGO.transform.localPosition = targetTile.transform.localPosition;
-                entityGO.GetComponent<MapEntity>().Position = entityGO.transform.localPosition;
+                    GameObject entityGO = Instantiate(entity);
+                    entityGO.transform.SetParent(Map.transform);
+                    entityGO.transform.localPosition = targetTile.transform.localPosition;
+                    entityGO.GetComponent<MapEntity>().Position = entityGO.transform.localPosition;
 
-                targetTile.EntityInTile = entityGO.GetComponent<MapEntity>();
-                entities.Add(entityGO.GetComponent<MapEntity>());
+                    targetTile.EntityInTile = entityGO.GetComponent<MapEntity>();
+                    entities.Add(entityGO.GetComponent<MapEntity>());
 
+                }
             }
-
         }
     }
 
     private void PrepareScenarioEffects()
     {
         // Get scenario effect if any
-        if(GameConfiguration.Instance.Level.CurrentScenario.ScenarioEffect)
-        {            
+        if (GameConfiguration.Instance.Level.CurrentScenario.ScenarioEffect)
+        {
             GameObject ScenarioEffect = Instantiate(GameConfiguration.Instance.Level.CurrentScenario.ScenarioEffect);
             scenarioEffects.Add(ScenarioEffect.GetComponent<ScenarioEffect>());
         }
 
         // Get scenario decoration tiles
-    }   
+    }
 
 
-#region Properties
+    #region Properties
     public int Turn
     {
         get { return _turn; }
@@ -172,5 +179,5 @@ public class ExploreGameController : MonoBehaviour {
         get { return scenarioEffects; }
     }
 
-#endregion
+    #endregion
 }
