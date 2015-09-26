4b4096646e2d19808726bd4bbd83cfb92cf39316
xipeluife <l.felipe.torres.p@gmail.com>
2015-09-26 16:59:12 -0500

    Cables entity and improved fb and twitter button res

---
+++ Assets/Scripts/MapEntities/CablesEntity.cs
@@ -0,0 +1,63 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Describes the trash behaviour inside the map
+/// </summary>
+public class CablesEntity : MapEntity
+{
+    public float DeathProbability = 1.0f;
+
+    public Vector2 spawnFrequencyLimits;
+
+    // Game Over properties
+    public Sprite DeathSprite;
+    public string DeathText;
+
+    // Audio related to effect
+    public AudioClip DeathFX;
+
+    public Tip tip;
+
+    /// <summary>
+    /// Function activated upon this entity's turn
+    /// </summary>
+    protected override IEnumerator PlayTurnRoutine()
+    {
+        InTurn = true;
+
+        // Effects
+        yield return null;
+        // Check for effects movement and ambient
+
+        InTurn = false;
+    }
+
+    /// <summary>
+    /// Activated when other entity walks onto this entity
+    /// </summary>
+    public override void ActivateEffect(MapEntity otherEntity)
+    {
+        // When walked on, reduce that entity life
+        if (otherEntity.GetType() == typeof(PlayerEntity))
+        {            
+            // Probability check
+            float prob = Random.Range(0.0f, 1.0f);
+
+            if(prob <= DeathProbability)
+            {
+                // Play audio effect
+                AudioManager.Instance.Play(AudioManager.AudioType.FX, DeathFX);
+
+                if(!(otherEntity as PlayerEntity).Tips.Exists(x => (x.Id == tip.Id)))
+                {
+                    (otherEntity as PlayerEntity).Tips.Add(tip);
+                }
+
+                // Kill player
+                (otherEntity as PlayerEntity).Kill(DeathSprite, DeathText);	
+            }
+        }
+    }
+}
